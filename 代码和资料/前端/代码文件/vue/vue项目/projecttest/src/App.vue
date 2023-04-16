00329b4e454c21ed7932610844fdede77689ea38
djmnb <1632782287@qq.com>
2023-04-16 21:45:20 +0800

    打卡

--- 代码和资料/前端/代码文件/vue/vue项目/projecttest/src/App.vue
+++ 代码和资料/前端/代码文件/vue/vue项目/projecttest/src/App.vue
@@ -1,35 +1,46 @@
 <template>
-  <div>
-    {{ test }}
-      <button @click="change">修改</button>
+  <div >
+    <VueMarkdown class="markdown-body">
+      {{ content }}
+    </VueMarkdown>
   </div>
 </template>
 
 <script>
+import VueMarkdown from "vue-markdown";
+
 export default {
   name: "app",
-  components: {},
+  components: { VueMarkdown },
   data() {
-    let t = {
-      test: {
-        b:"fsdfds"
-      }
-    }
-    console.log(t)
-    return t
+    return {
+      content: `
+    
+    当然可以,以下是一个简单的C++ HelloWorld程序:
+
+\`\`\` c++
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+\`\`\` 
+    `,
+    };
   },
   methods: {
     change() {
       let t = {
-        a:'111'
-      }
-      console.log("aaa",t)
-      this.test = t
-      
-    }
-  }
+        a: "111",
+      };
+      console.log("aaa", t);
+      this.test = t;
+    },
+  },
 };
 </script>
 
 <style>
+@import 'github-markdown-css/github-markdown.css';
 </style>
