489c562d18ea7a18df27cc8503f426aa34930dd6
djmnb <1632782287@qq.com>
2023-08-20 22:09:08 +0800

    考研打卡

---
+++ 代码和资料/数据结构/test/02-强制类型转换.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = -1;
+    // unsigned int b = a; // b = 2^32-1
+    // cout << a << " " << b << endl;
+
+    int a = (int)0b11000000000000000000000000000000;
+    short b = (short)a;
+    cout << a << " " << b << endl;
+
+
+    int a = 0x01234567
+    01 23 45 67
+    67 45 23 01
+}
